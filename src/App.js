6d00a872390bbae9ed4774937b586db12d9031ab
Abdul Rehman <amcount000@gmail.com>
2025-02-04 01:08:39 +0530

    Timer App Project Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TimerApp from './TimerApp';
+
+function App() {
+  return (
+    <div>
+      <TimerApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TimerApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TimerApp() {
+	return(<>
+	<h1>Timer App</h1>
+	</>);
+}
+
+export default TimerApp;
